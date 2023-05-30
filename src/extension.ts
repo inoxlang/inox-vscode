bfaa5fb3ef0a9adc3d9ae79a9c3d25fa3625aa2b
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-05-30 23:14:09 +0200

    LSP: support websocket

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,9 @@
 import * as vscode from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions, StreamInfo, TransportKind, Executable } from 'vscode-languageclient/node';
+import { LanguageClient, LanguageClientOptions, ServerOptions, MessageTransports } from 'vscode-languageclient/node';
+import { listen, WebSocketMessageReader, WebSocketMessageWriter, toSocket, ConsoleLogger } from './vscode-ws-jsonrpc/src/index'
+
+import { WebSocket as _Websocket } from 'ws';
+import { inspect } from 'util';
 
 let client: LanguageClient;
 let outputChannel: vscode.OutputChannel;
@@ -7,11 +11,10 @@ let outputChannel: vscode.OutputChannel;
 export function activate(context: vscode.ExtensionContext): void {
   outputChannel = vscode.window.createOutputChannel('Inox Extension');
 
-  const serverOptions: ServerOptions = {
-    command: 'inox',
-    args: ['lsp'],
+  const config = vscode.workspace.getConfiguration('inox')
+  const useInoxBinary = config.get('useInoxBinary') === true
+  const serverOptions = getLspServerOptions(useInoxBinary)
 
-  };
 
   const clientOptions: LanguageClientOptions = {
     documentSelector: [{ scheme: 'file', language: 'inox' }],
@@ -32,4 +35,51 @@ export function deactivate(): Thenable<void> | undefined {
     return undefined;
   }
   return client.stop();
+}
+
+function getLspServerOptions(useInoxBinary: boolean): ServerOptions {
+  if(useInoxBinary) {
+    outputChannel.appendLine('use inox binary')
+    return {
+      command: 'inox',
+      args: ['lsp'],
+    };
+  }
+
+  outputChannel.appendLine('use websocket')
+  return connectToWebsocketServer(outputChannel)
+}
+
+
+export function connectToWebsocketServer(outputChannel: vscode.OutputChannel): () => Promise<MessageTransports> {
+  return async () => {
+    outputChannel.appendLine('create websocket')
+
+    const webSocket = new _Websocket('wss://localhost:8888', {
+      rejectUnauthorized: false,
+    }) as any as WebSocket;
+
+    webSocket.onerror = ev => {
+      outputChannel.appendLine(inspect(ev))
+    }
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        outputChannel.appendLine('timeout')
+        reject()
+      }, 1000)
+
+      webSocket.onopen = () => {
+        outputChannel.appendLine('websocket connected')
+        const socket = toSocket(webSocket);
+        const reader = new WebSocketMessageReader(socket);
+        const writer = new WebSocketMessageWriter(socket);
+        resolve({
+          reader,
+          writer,
+        })
+      }
+    })
+  }
+
 }
