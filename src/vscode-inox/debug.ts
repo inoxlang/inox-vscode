679c0e217ff91df0673971b266df52a83fff5886
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-06-22 20:20:17 +0200

    lsp: remove worker code

--- src/vscode-inox/debug.ts
+++
@@ -1,9 +0,0 @@
-import { parentPort } from "worker_threads"
-
-export const printDebug = (...args: string[]) => {
-    parentPort!.postMessage({ method: 'print_debug', id: Math.random(), args: args })
-}
-
-export const printTrace = (...args: string[]) => {
-    parentPort!.postMessage({ method: 'print_trace', id: Math.random(), args: args })
-}
