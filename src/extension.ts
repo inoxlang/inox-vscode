2bf33a17808f08520724b805fcab76c7f5c4a077
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-09-23 20:46:15 +0200

    change identifier of commands

--- src/extension.ts
+++ src/extension.ts
@@ -15,70 +15,71 @@ let debugChannel: vscode.OutputChannel;
 
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
-  outputChannel = vscode.window.createOutputChannel('Inox Extension');
-  outputChannel.appendLine('Inox extension activate()')
+    outputChannel = vscode.window.createOutputChannel('Inox Extension');
+    outputChannel.appendLine('Inox extension activate()')
 
-  debugChannel = vscode.window.createOutputChannel('Inox Extension (Debug)');
+    debugChannel = vscode.window.createOutputChannel('Inox Extension (Debug)');
 
-  const config = await getConfiguration(outputChannel)
-  if (!config) {
-    return
-  }
-
-  const isVirtualWorkspace =
-    vscode.workspace.workspaceFolders != undefined &&
-    vscode.workspace.workspaceFolders.every(f => f.uri.scheme !== 'file');
-
-  if (isVirtualWorkspace) {
-    vscode.window.showErrorMessage('virtual workspaces not supported yet')
-    return
-  }
-
-  const ctx = new InoxExtensionContext({
-    base: context,
-    initialConfig: config,
-    virtualWorkspace: isVirtualWorkspace,
-    outputChannel: outputChannel,
-    debugChannel: debugChannel,
-
-    getCurrentConfig: getConfiguration,
-    createLSPClient: createLSPClient,
-    startLocalProjectServerIfNecessary: startLocalProjectServerIfNecessary,
-    openProject: openProject
-  })
-
-  if (config.project) {
-    ctx.inoxFS = createAndRegisterInoxFs(ctx)
-    const secretKeeper = new SecretKeeper(ctx);
-    vscode.window.registerTreeDataProvider('secretKeeper', secretKeeper);
-    vscode.commands.registerCommand('secretKeeper.addEntry', secretKeeper.addSecret.bind(secretKeeper))
-    vscode.commands.registerCommand('secretKeeper.deleteEntry', (entry: SecretEntry) => secretKeeper.deleteSecret(entry.label))
-    vscode.commands.registerCommand('secretKeeper.updateEntry', (entry: SecretEntry) => secretKeeper.updateSecret(entry.label))
-  }
-
-  ctx.restartLSPClient(false)
-
-  //register formatting provider
-
-  vscode.languages.registerDocumentFormattingEditProvider([{ scheme: INOX_FS_SCHEME, language: 'inox' }], {
-    provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
-      if (ctx.lspClient === undefined) {
-        return []
-      }
-
-      const params: DocumentFormattingParams = {
-        textDocument: TextDocumentIdentifier.create(document.uri.toString()),
-        options: options,
-      }
-
-      return ctx.lspClient.sendRequest('textDocument/formatting', params)
+    const config = await getConfiguration(outputChannel)
+    if (!config) {
+        return
     }
-  })
+
+    const isVirtualWorkspace =
+        vscode.workspace.workspaceFolders != undefined &&
+        vscode.workspace.workspaceFolders.every(f => f.uri.scheme !== 'file');
+
+    if (isVirtualWorkspace) {
+        vscode.window.showErrorMessage('virtual workspaces not supported yet')
+        return
+    }
+
+    const ctx = new InoxExtensionContext({
+        base: context,
+        initialConfig: config,
+        virtualWorkspace: isVirtualWorkspace,
+        outputChannel: outputChannel,
+        debugChannel: debugChannel,
+
+        getCurrentConfig: getConfiguration,
+        createLSPClient: createLSPClient,
+        startLocalProjectServerIfNecessary: startLocalProjectServerIfNecessary,
+        openProject: openProject
+    })
+
+    if (config.project) {
+        ctx.inoxFS = createAndRegisterInoxFs(ctx)
+
+        const secretKeeper = new SecretKeeper(ctx);
+        vscode.window.registerTreeDataProvider('secretKeeper', secretKeeper);
+        vscode.commands.registerCommand('inox.secretKeeper.addEntry', secretKeeper.addSecret.bind(secretKeeper))
+        vscode.commands.registerCommand('inox.secretKeeper.deleteEntry', (entry: SecretEntry) => secretKeeper.deleteSecret(entry.label))
+        vscode.commands.registerCommand('inox.secretKeeper.updateEntry', (entry: SecretEntry) => secretKeeper.updateSecret(entry.label))
+    }
+
+    ctx.restartLSPClient(false)
+
+    //register formatting provider
+
+    vscode.languages.registerDocumentFormattingEditProvider([{ scheme: INOX_FS_SCHEME, language: 'inox' }], {
+        provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
+            if (ctx.lspClient === undefined) {
+                return []
+            }
+
+            const params: DocumentFormattingParams = {
+                textDocument: TextDocumentIdentifier.create(document.uri.toString()),
+                options: options,
+            }
+
+            return ctx.lspClient.sendRequest('textDocument/formatting', params)
+        }
+    })
 
 
-  //register debug adapter
+    //register debug adapter
 
-  context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('inox', new InlineDebugAdapterFactory(ctx)));
+    context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('inox', new InlineDebugAdapterFactory(ctx)));
 
   //register commands
 
@@ -87,38 +88,47 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
     return ctx.restartLSPClient(false)
   })
 
-  vscode.commands.registerCommand('project/initialize', async () => {
-    await ctx.updateConfiguration()
+    //register commands
+    {
+        vscode.commands.registerCommand('inox.lsp.restart', async () => {
+            await ctx.updateConfiguration()
+            return ctx.restartLSPClient(false)
+        })
 
-    if (ctx.config.projectFilePresent) {
-      const msg = '[project/initialize] project is already initialized'
-      vscode.window.showWarningMessage(msg)
-      ctx.debugChannel.appendLine(msg)
-      return
+        vscode.commands.registerCommand('inox.project.initialize', async () => {
+            await ctx.updateConfiguration()
+
+            if (ctx.config.projectFilePresent) {
+                const msg = '[project/initialize] project is already initialized'
+                vscode.window.showWarningMessage(msg)
+                ctx.debugChannel.appendLine(msg)
+                return
+            }
+
+            debugChannel.appendLine('[project/initialize] restart LSP client in project mode')
+            await ctx.restartLSPClient(true) //restart LSP client in project mode
+
+            await vscode.window.withProgress({
+                location: vscode.ProgressLocation.Window,
+                cancellable: false,
+                title: 'Creating Inox project in current folder'
+            }, async (progress) => {
+                progress.report({ increment: 0 });
+
+                //wait for LSP client
+                await sleep(3000) //TODO: replace sleep
+
+                await initializeNewProject(ctx)
+
+                progress.report({ increment: 100 });
+            });
+        })
     }
 
-    debugChannel.appendLine('[project/initialize] restart LSP client in project mode')
-    await ctx.restartLSPClient(true) //restart LSP client in project mode
-
-    await vscode.window.withProgress({
-      location: vscode.ProgressLocation.Window,
-      cancellable: false,
-      title: 'Creating Inox project in current folder'
-    }, async (progress) => {
-      progress.report({ increment: 0 });
-
-      //wait for LSP client
-      await sleep(3000) //TODO: replace sleep
-
-      await initializeNewProject(ctx)
-
-      progress.report({ increment: 100 });
-    });
-  })
 
 }
 
 export function deactivate(): Thenable<void> | undefined {
-  return
+    return
 }
 
