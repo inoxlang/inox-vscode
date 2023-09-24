4d0e2db094c4cad3baf503595d7ca2004c96271d
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-09-25 00:13:39 +0200

    project: generate README.txt

--- src/project.ts
+++ src/project.ts
@@ -2,31 +2,37 @@ import * as fs from 'fs'
 import * as vscode from 'vscode'
 
 import { InoxExtensionContext } from "./inox-extension-context";
-import { join } from 'path';
+import { join, basename } from 'path';
 import { stringifyCatchedValue } from './utils';
 import { saveTempTokens } from './configuration';
 import { LSP_CLIENT_NOT_RUNNING_MSG } from './errors';
 
 
-export async function initializeNewProject(ctx: InoxExtensionContext, projectName: string){
+export async function initializeNewProject(ctx: InoxExtensionContext, projectName: string) {
     const lspClient = ctx.lspClient
-    if(!lspClient || !lspClient.isRunning()){
+    if (!lspClient || !lspClient.isRunning()) {
         throw new Error(LSP_CLIENT_NOT_RUNNING_MSG)
     }
 
-    if(ctx.config.project?.id){
+    if (ctx.config.project?.id) {
         vscode.window.showWarningMessage('project is already initialized')
         return
     }
 
     let initialLaunchConfigurations: any[]
     try {
-        initialLaunchConfigurations = 
+        initialLaunchConfigurations =
             ctx.base.extension.packageJSON.contributes.debuggers[0].initialConfigurations
     } catch {
         vscode.window.showWarningMessage('failed to get initial launch configurations')
         return
     }
+
+
+    const localProjectRoot = ctx.fileWorkspaceFolder.uri.path
+    const workspaceFile = join(localProjectRoot, `${ctx.fileWorkspaceFolder.name}.code-workspace`)
+    const inoxProjectFile = join(localProjectRoot, 'inox-project.json')
+    const readmeFile = join(localProjectRoot, 'README.txt')
 
     const workspaceFileContent = {
         "folders": [
@@ -36,7 +42,7 @@ export async function initializeNewProject(ctx: InoxExtensionContext, projectNam
             },
             {
                 "path": ".",
-                "name": `Local Folder '${ctx.fileWorkspaceFolder.name}' - do not add or change files`
+                "name": ctx.fileWorkspaceFolder.name
             }
         ],
         "settings": {
@@ -49,28 +55,34 @@ export async function initializeNewProject(ctx: InoxExtensionContext, projectNam
         }
     }
 
+    const readmeFileContent = [
+        'Please do not edit or add files here. This folder should only contain local configuration.',
+        `You can open the Inox project by opening '${basename(workspaceFile)}' and clicking the [Open Workspace] button in the bottom right corner.`,
+        '',
+        'Alternatively you can open the workspace file by going in the VSCode menu / File / Open Workspace from File.',
+        'The next time you can directly click on File / Open Recent / ... (Workspace).'
+    ].join('\n')
 
     const inoxProjectFileContent: Record<string, unknown> = {}
 
-    const localProjectRoot = ctx.fileWorkspaceFolder.uri.path
-    const workspaceFile = join(localProjectRoot, `${ctx.fileWorkspaceFolder.name}.code-workspace`)
-    const inoxProjectFile = join(localProjectRoot, 'inox-project.json')
-
-
-    if(! fs.existsSync(workspaceFile)){
+    if (!fs.existsSync(workspaceFile)) {
         fs.writeFileSync(workspaceFile, JSON.stringify(workspaceFileContent, null, '  '))
     }
-    
-    if(! fs.existsSync(inoxProjectFile)){
+
+    if (!fs.existsSync(readmeFile)) {
+        fs.writeFileSync(readmeFile, readmeFileContent)
+    }
+
+    if (!fs.existsSync(inoxProjectFile)) {
         try {
-            const projectId = await lspClient.sendRequest('project/create', {name: projectName})
-            if(typeof projectId != 'string'){
+            const projectId = await lspClient.sendRequest('project/create', { name: projectName })
+            if (typeof projectId != 'string') {
                 throw new Error('project ID returned by LSP server should be a string but is a(n) ' + (typeof projectId))
             }
             inoxProjectFileContent.id = projectId
             fs.writeFileSync(inoxProjectFile, JSON.stringify(inoxProjectFileContent, null, ' '))
-    
-        } catch(err) {
+
+        } catch (err) {
             const msg = stringifyCatchedValue(err)
             ctx.outputChannel.appendLine(msg)
             vscode.window.showErrorMessage(msg)
@@ -83,13 +95,13 @@ export async function initializeNewProject(ctx: InoxExtensionContext, projectNam
 
 export async function openProject(ctx: InoxExtensionContext) {
     const lspClient = ctx.lspClient
-    if(!lspClient || !lspClient.isRunning()){
+    if (!lspClient || !lspClient.isRunning()) {
         throw new Error(LSP_CLIENT_NOT_RUNNING_MSG)
     }
 
     const projectId = ctx.config.project?.id
 
-    if(!projectId){
+    if (!projectId) {
         vscode.window.showWarningMessage('failed to open project: missing .id in project configuration')
         return
     }
@@ -97,19 +109,19 @@ export async function openProject(ctx: InoxExtensionContext) {
     try {
         ctx.debugChannel.appendLine("Send 'project/open' request")
         const resp = await lspClient.sendRequest('project/open', {
-            projectId: projectId, 
+            projectId: projectId,
             config: ctx.config.project ?? {},
             tempTokens: ctx.config.tempTokens
         })
 
-        if((typeof resp != 'object') || resp === null){
+        if ((typeof resp != 'object') || resp === null) {
             vscode.window.showErrorMessage('invalid response from project server (method: project/open)')
             return
         }
-        await saveTempTokens(ctx, (resp as Record<string,unknown>).tempTokens)
+        await saveTempTokens(ctx, (resp as Record<string, unknown>).tempTokens)
 
         ctx.projectOpen = true
-    } catch(err){
+    } catch (err) {
         vscode.window.showErrorMessage(stringifyCatchedValue(err))
     }
 }
