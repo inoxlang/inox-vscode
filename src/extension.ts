086c12546a7f0836b526bbc7a7804de61694fc48
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-10-03 11:16:50 +0200

    project: minor cleanup
    Move logic for inox.project.initialize in project.ts

--- src/extension.ts
+++ src/extension.ts
@@ -11,8 +11,6 @@ import { createLSPClient, startLocalProjectServerIfNecessary } from './lsp';
 import { initializeNewProject } from './project';
 import { SecretEntry, SecretKeeper } from './project/secret-keeper';
 import { computeSuggestions } from './suggestions';
-import { sleep } from './utils';
-const PROJECT_NAME_REGEX = /^[a-z][a-z0-9_-]+$/i
 
 let outputChannel: vscode.OutputChannel;
 let debugChannel: vscode.OutputChannel;
@@ -100,77 +98,7 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
         })
 
         vscode.commands.registerCommand('inox.project.initialize', async () => {
-            await ctx.updateConfiguration()
-
-            if (ctx.config.projectFilePresent) {
-                const msg = '[project/initialize] project is already initialized'
-                vscode.window.showWarningMessage(msg)
-                ctx.debugChannel.appendLine(msg)
-                return
-            }
-
-
-            let projectName: string
-            {
-
-                let input: string | undefined
-                let tries = 2
-
-                while (tries > 0 && input === undefined) {
-                    tries--
-                    input = await vscode.window.showInputBox({
-                        placeHolder: 'Examples: learn-inox, my-web-app',
-                        prompt: `Name of the project`,
-                        async validateInput(value) {
-                            if (PROJECT_NAME_REGEX.test(value)) {
-                                return null
-                            } else {
-                                return {
-                                    message: "The project's name should starts with a letter and should only contains letters, digits, '-' and '_'",
-                                    severity: vscode.InputBoxValidationSeverity.Error
-                                }
-                            }
-                        },
-                    })
-                }
-
-                if (input === undefined) {
-                    return
-                }
-                projectName = input
-            }
-
-
-            debugChannel.appendLine('[project/initialize] restart LSP client in project mode')
-            await ctx.restartLSPClient(true) //restart LSP client in project mode
-
-            await vscode.window.withProgress({
-                location: vscode.ProgressLocation.Window,
-                cancellable: false,
-                title: 'Creating Inox project in current folder'
-            }, async (progress) => {
-                progress.report({ increment: 0 });
-
-                //wait for LSP client
-                for (let i = 0; i < 3; i++) {
-                    if (!ctx.lspClient?.isRunning()) {
-                        await sleep(1000)
-                    }
-                }
-
-                if (!ctx.lspClient?.isRunning()) {
-                    progress.report({ increment: 100 });
-                    vscode.window.showErrorMessage(LSP_CLIENT_NOT_RUNNING_MSG)
-                }
-
-                await initializeNewProject(ctx, projectName)
-
-                vscode.window.showInformationMessage(
-                    "The project should have been created on the server. You can open it by clicking 'Open Workspace' in the `.code-workspace` file."
-                )
-
-                progress.report({ increment: 100 });
-            });
+            return initializeNewProject(ctx)
         })
     }
 
--- src/project.ts
+++ src/project.ts
@@ -6,8 +6,10 @@ import { join, basename } from 'path';
 import { stringifyCatchedValue } from './utils';
 import { saveTempTokens } from './configuration';
 import { LSP_CLIENT_NOT_RUNNING_MSG } from './errors';
+import { sleep } from './utils';
 
 const PROJECT_KEY_PREFIX = 'project/'
+const PROJECT_NAME_REGEX = /^[a-z][a-z0-9_-]+$/i
 
 function getStateValue(ctx: InoxExtensionContext, key: string) {
     return ctx.getStateValue(PROJECT_KEY_PREFIX + key)
@@ -17,7 +19,82 @@ function setStateValue(ctx: InoxExtensionContext, key: string, value: unknown) {
     return ctx.setStateValue(PROJECT_KEY_PREFIX + key, value)
 }
 
-export async function initializeNewProject(ctx: InoxExtensionContext, projectName: string) {
+
+export async function initializeNewProject(ctx: InoxExtensionContext) {
+    await ctx.updateConfiguration()
+
+    if (ctx.config.projectFilePresent) {
+        const msg = '[project/initialize] project is already initialized'
+        vscode.window.showWarningMessage(msg)
+        ctx.debugChannel.appendLine(msg)
+        return
+    }
+
+
+    let projectName: string
+    {
+
+        let input: string | undefined
+        let tries = 2
+
+        while (tries > 0 && input === undefined) {
+            tries--
+            input = await vscode.window.showInputBox({
+                placeHolder: 'Examples: learn-inox, my-web-app',
+                prompt: `Name of the project`,
+                async validateInput(value) {
+                    if (PROJECT_NAME_REGEX.test(value)) {
+                        return null
+                    } else {
+                        return {
+                            message: "The project's name should starts with a letter and should only contains letters, digits, '-' and '_'",
+                            severity: vscode.InputBoxValidationSeverity.Error
+                        }
+                    }
+                },
+            })
+        }
+
+        if (input === undefined) {
+            return
+        }
+        projectName = input
+    }
+
+
+    ctx.debugChannel.appendLine('[project/initialize] restart LSP client in project mode')
+    await ctx.restartLSPClient(true) //restart LSP client in project mode
+
+    await vscode.window.withProgress({
+        location: vscode.ProgressLocation.Window,
+        cancellable: false,
+        title: 'Creating Inox project in current folder'
+    }, async (progress) => {
+        progress.report({ increment: 0 });
+
+        //wait for LSP client
+        for (let i = 0; i < 3; i++) {
+            if (!ctx.lspClient?.isRunning()) {
+                await sleep(1000)
+            }
+        }
+
+        if (!ctx.lspClient?.isRunning()) {
+            progress.report({ increment: 100 });
+            vscode.window.showErrorMessage(LSP_CLIENT_NOT_RUNNING_MSG)
+        }
+
+        await _initializeNewProject(ctx, projectName)
+
+        vscode.window.showInformationMessage(
+            "The project should have been created on the server. You can open it by clicking 'Open Workspace' in the `.code-workspace` file."
+        )
+
+        progress.report({ increment: 100 });
+    });
+}
+
+async function _initializeNewProject(ctx: InoxExtensionContext, projectName: string) {
     const lspClient = ctx.lspClient
     if (!lspClient || !lspClient.isRunning()) {
         throw new Error(LSP_CLIENT_NOT_RUNNING_MSG)
@@ -86,7 +163,7 @@ export async function initializeNewProject(ctx: InoxExtensionContext, projectNam
         const isFirstProject = getStateValue(ctx, 'first-created') !== true
 
         try {
-            const projectId = await lspClient.sendRequest('project/create', { 
+            const projectId = await lspClient.sendRequest('project/create', {
                 name: projectName,
                 addTutFile: isFirstProject
             })
