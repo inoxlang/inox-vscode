789d9eab5769bf99215e9920c033a504c49de81f
GraphR00t <113632189+GraphR00t@users.noreply.github.com>
2023-12-15 18:21:58 +0100

    project: update creation success message

--- src/project/create.ts
+++ src/project/create.ts
@@ -95,7 +95,8 @@ export async function initializeNewProject(ctx: InoxExtensionContext) {
         await _initializeNewProject(ctx, projectName)
 
         vscode.window.showInformationMessage(
-            "The project should have been created on the server. You can open it by clicking 'Open Workspace' in the `<project>.code-workspace` file.",
+            "The project should have been created on the server. "+
+            `You can open it by clicking 'Open Workspace' in the '${projectName}.code-workspace' file.`,
             {
                 modal: true,
                 detail: "If some extensions show an error when the workspace is loaded you can disable them in it."
